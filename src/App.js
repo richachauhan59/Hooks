7336f8f5ac839dec21a837c877df156210ac4c3d
richachauhan59 <richachuahan2019@gmail.com>
2020-10-08 19:33:22 +0530

    ToDo Using Hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Github from './Components/Github'
 import CounterHooks from './Components/CounterHooks'
+import Todo from './ToDo_Hooks/todo'
 
 function App() {
   return (
     <div className="App">
       <CounterHooks/>
       <Github/>
+      <Todo/>
     </div>
   );
 }
--- src/ToDo_Hooks/todo.jsx
+++ src/ToDo_Hooks/todo.jsx
@@ -1,16 +1,30 @@
-import React , { useState } from 'react'
-import axios from 'axios'
+import React , { useState, useEffect
+ } from 'react'
+import { v4 as uuid} from 'uuid'
 
 function ToDo(){
     const [query, setQuery] = useState("")
-    const [data, setData] = useState(null)
+    const [data, setData] = useState([])
 
 
-    handleClick = ( ) => {
-        data:
+    useEffect(() => {
+        handleClick();
+      }, []);
+    
+      useEffect(() => {
+      }, [data, query]);
+
+
+    const handleClick = ( ) => {
+        setData([...data, {title: query, id: uuid()}])
     }
 
- 
+
+    const deleteTodo = (id) => {
+        setData(data.filter((data) => data.id !== id));
+      };
+    
+      console.log(data)
     return (
         <div style={{padding:"2%", border:"2px solid black" , width:"50%", marginLeft:"20%",marginBottom:"9%", borderRadius:"5px", marginTop:"5%"}}>
             <h1> ToDo </h1>
@@ -18,10 +32,12 @@ function ToDo(){
             onChange={(e) => setQuery(e.target.value)}
             />
             <button onClick={handleClick} >Add</button>
-            <button  onClick={handleClick} >Delete</button>
 
-            {data && data.map(item => <div key={item.id} > {item.login} </div> ) }
-
+            {data && data.map((item, index) => 
+            <div id={item.id}> {item.title}
+            <button  onClick={() => deleteTodo(item.id)} >Delete</button>
+            </div>
+            )}
         </div>
     )
 }
